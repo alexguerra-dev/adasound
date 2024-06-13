1d9a7423a65c8e9e02b0fce804af0eded997a6bc
Alex Guerra <alexander.a.guerra@gmail.com>
2024-06-13 14:10:57 -0500

    chore: Add footer component and update navigation in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,71 @@
 import Link from 'next/link'
+import Footer from './ui/footer'
 
 export default function Home() {
     return (
         <main>
             <h1>Adasound</h1>
-            <Link href="resume">Resume</Link>
+
+            <nav className="bg-gray-800 text-5xl">
+                <ul>
+                    <li>
+                        <Link
+                            className="text-white hover:text-red-300
+                            p-4"
+                            href="resume"
+                        >
+                            Resume
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="text-white hover:text-red-300
+                            p-4"
+                            href="projects"
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="text-white hover:text-red-300
+                            p-4"
+                            href="about"
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="text-white hover:text-red-300
+                            p-4"
+                            href="blog"
+                        >
+                            Blog
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="text-white hover:text-red-300
+                            p-4"
+                            href="legal"
+                        >
+                            Legal
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="text-white hover:text-red-300
+                            p-4"
+                            href="resume"
+                        >
+                            Resume
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Footer></Footer>
+            {/* <Footer></Footer> */}
         </main>
     )
 }
